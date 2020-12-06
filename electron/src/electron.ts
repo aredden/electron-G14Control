e45392de8f9289c6c7981750e8a94d106f7aa04e
Alex Redden <alexander.h.redden@gmail.com>
2020-12-06 10:56:12 -0500

    Fixed an issue with browserWindow not opening & hid undefined error on close if not bound.

--- electron/src/electron.ts
+++ electron/src/electron.ts
@@ -5,18 +5,31 @@ import {
 	BrowserWindow,
 	Menu,
 	Tray,
-	Notification,
 	globalShortcut,
 	powerMonitor,
+	ipcMain,
 } from 'electron';
 
 import getLogger from './Logger';
 import path from 'path';
 import is_dev from 'electron-is-dev';
-import { killEmitters } from './IPCEvents/IPCEmitters';
-import { createWindow } from './Browser';
+import {
+	buildEmitters,
+	killEmitters,
+	loopsAreRunning,
+	runLoop,
+} from './IPCEvents/IPCEmitters';
 import { HID } from 'node-hid';
-
+import { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
+import url from 'url';
+import { loadConfig } from './IPCEvents/ConfigLoader';
+import { setUpNewG14ControlKey } from './IPCEvents/HID/HIDDevice';
+import { buildIpcConnection } from './IPCEvents/IPCListeners';
+import { mapperBuilder } from './IPCEvents/RogKeyRemapperListener';
+import { buildTrayIcon } from './TrayIcon';
+import installExtension from 'electron-devtools-installer';
+import forceFocus from 'forcefocus';
+const LOGGER = getLogger('Main');
 const gotTheLock = app.requestSingleInstanceLock();
 
 export let g14Config: G14Config;
@@ -25,18 +38,8 @@ export const setG14Config = (g14conf: G14Config) => {
 };
 
 if (!gotTheLock) {
+	LOGGER.info('Another process had the lock, quitting!');
 	app.quit();
-} else {
-	app.on('second-instance', () => {
-		new Notification({
-			title: 'G14Control',
-			body: 'Only one instance of G14Control can be run at a time!',
-		}).show();
-		if (browserWindow) {
-			if (browserWindow.isMinimized()) browserWindow.restore();
-			browserWindow.focus();
-		}
-	});
 }
 
 const ICONPATH = is_dev
@@ -48,9 +51,8 @@ const ICONPATH = is_dev
 			'extraResources',
 			'icon.ico'
 	  );
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const LOGGER = getLogger('Main');
+
 export let browserWindow: BrowserWindow;
 export let showIconEnabled = false;
 export let tray: Tray;
@@ -66,7 +68,14 @@ export const minMaxFunc = () => {
 	if (browserWindow.isFocused()) {
 		browserWindow.minimize();
 	} else {
-		browserWindow.show();
+		LOGGER.info('Attempting to force focus to the browserWindow');
+		if (browserWindow) {
+			browserWindow.show();
+			forceFocus.focusWindow(browserWindow);
+			browserWindow.show();
+		} else {
+			LOGGER.info("browserWinow didn't exist.");
+		}
 	}
 };
 
@@ -87,6 +96,145 @@ export const updateMenuVisible = (minimized?: boolean) => {
 	}
 };
 
+export const buildBrowserListeners = (browserWindow: BrowserWindow) => {
+	// set up renderer process events.
+	browserWindow.on('hide', async () => {
+		if (loopsAreRunning()) {
+			await killEmitters();
+		}
+		updateMenuVisible();
+	});
+	browserWindow.on('show', () => {
+		if (!loopsAreRunning()) {
+			runLoop(browserWindow);
+		}
+		updateMenuVisible();
+	});
+	browserWindow.on('minimize', async () => {
+		if (loopsAreRunning()) {
+			await killEmitters();
+		}
+		updateMenuVisible();
+	});
+	browserWindow.on('restore', () => {
+		if (!loopsAreRunning()) {
+			runLoop(browserWindow);
+		}
+		updateMenuVisible();
+	});
+	browserWindow.on('unresponsive', () => {
+		LOGGER.info('Window is unresponsive...');
+		browserWindow.webContents.reload();
+	});
+};
+
+export async function createWindow(
+	gotTheLock: any,
+	g14Config: G14Config,
+	browserWindow: BrowserWindow,
+	tray: Tray,
+	trayContext: any,
+	ICONPATH: string,
+	hid: HID
+) {
+	// Create the browser window.
+	if (!gotTheLock) {
+		app.quit();
+	}
+
+	try {
+		g14Config = JSON.parse((await loadConfig()).toString()) as G14Config;
+	} catch (err) {
+		LOGGER.error('Error loading config at startup');
+	}
+
+	browserWindow = new BrowserWindow({
+		width: 960,
+		height: 600,
+		resizable: true,
+		maxWidth: is_dev ? 1920 : 1300,
+		minWidth: 960,
+		titleBarStyle: 'hidden',
+		autoHideMenuBar: true,
+		frame: false,
+		icon: ICONPATH,
+		webPreferences: {
+			allowRunningInsecureContent: false,
+			worldSafeExecuteJavaScript: true,
+			accessibleTitle: 'G14ControlV2',
+			preload: __dirname + '/Preload.js',
+		},
+		darkTheme: true,
+	});
+	if (is_dev) {
+		installExtension(REACT_DEVELOPER_TOOLS)
+			.then((name: any) => console.log(`Added Extension:  ${name}`))
+			.catch((err: any) =>
+				console.log('An error occurred adding devtools: ', err)
+			);
+	}
+	const ipc = ipcMain;
+	ipc.setMaxListeners(35);
+	buildIpcConnection(ipc, browserWindow);
+	buildEmitters(ipc, browserWindow);
+	// and load the index.html of the app.
+	let loadurl = 'http://localhost:3000/';
+	if (!is_dev) {
+		browserWindow.loadURL(
+			url.format({
+				pathname: path.join(__dirname, './index.html'),
+				protocol: 'file:',
+				slashes: true,
+			})
+		);
+	} else {
+		browserWindow.loadURL(loadurl);
+	}
+
+	// build tray icon menu
+	let results = await buildTrayIcon(tray, trayContext, browserWindow);
+
+	tray = results.tray;
+	trayContext = results.trayContext;
+	browserWindow = results.browserWindow;
+
+	buildBrowserListeners(browserWindow);
+
+	let { shortcuts, rogKey } = g14Config.current;
+
+	// Register global shortcut ctrl + space
+	if (shortcuts.minmax.enabled) {
+		let registered = globalShortcut.register(
+			shortcuts.minmax.accelerator,
+			() => {
+				if (browserWindow.isFocused()) {
+					browserWindow.minimize();
+				} else {
+					browserWindow.show();
+				}
+			}
+		);
+		if (registered) {
+			LOGGER.info('Show app shortcut registered.');
+		} else {
+			LOGGER.info('Error registering shortcut.');
+		}
+	}
+
+	if (rogKey.enabled) {
+		let hdd = setUpNewG14ControlKey(mapperBuilder);
+		if (hdd) {
+			setHidMain(hdd);
+			LOGGER.info('ROG key HID built and listening.');
+		} else {
+			LOGGER.info('There was an issue setting up ROG HID');
+		}
+	}
+	browserWindow.setMenu(null);
+	app.setAppLogsPath(app.getPath('home') + '/g14control_logs');
+	return { tray, browserWindow, g14Config, trayContext };
+}
+
 powerMonitor.on('shutdown', () => {
 	app.quit();
 });
@@ -100,6 +248,9 @@ app.on('window-all-closed', async () => {
 
 app.on('before-quit', async () => {
 	globalShortcut.unregisterAll();
+	if (hid) {
+		hid.close();
+	}
 	LOGGER.info('Keyboard shortcuts unregistered.');
 	LOGGER.info('Preparing to quit.');
 	killEmitters();
@@ -107,6 +258,7 @@ app.on('before-quit', async () => {
 
 app.on('quit', async (evt, e) => {
 	LOGGER.info(`Quitting with exit code. ${e}`);
+	process.exit();
 });
 
 app.on('ready', async () => {
@@ -119,7 +271,6 @@ app.on('ready', async () => {
 		ICONPATH,
 		hid
 	);
-
 	g14Config = results.g14Config;
 	tray = results.tray;
 	browserWindow = results.browserWindow;
@@ -138,4 +289,12 @@ app.on('renderer-process-crashed', (event, webcontents, killed) => {
 
 app.on('render-process-gone', (event, contents, details) => {
 	LOGGER.info(`Renderer Process is Gone:\n${details.reason}`);
+});
+
+app.on('second-instance', () => {
+	LOGGER.info('Another process interupted my thoughts, how dare he!');
+	if (browserWindow) {
+		if (browserWindow.isMinimized()) browserWindow.restore();
+		browserWindow.focus();
+	}
 });
