2ec5611f4622b89588eac204db519dbafc2c4a1b
Alex Redden <alexander.h.redden@gmail.com>
2020-12-15 12:06:50 -0500

    testing update

--- electron/src/AppUpdater.ts
+++ electron/src/AppUpdater.ts
@@ -4,10 +4,16 @@ import { BrowserWindow } from 'electron';
 import { autoUpdater } from 'electron-updater';
 import getLogger from './Logger';
 
+let LOGGER = getLogger('AutoUpdater');
+
 export default class AppUpdater {
 	constructor(browserWindow: BrowserWindow) {
 		const LOGGER = getLogger('AppUpdater');
-		autoUpdater.checkForUpdatesAndNotify();
+		autoUpdater.allowPrerelease = true;
+		LOGGER.info(autoUpdater.getFeedURL());
+		autoUpdater.checkForUpdatesAndNotify().then((val) => {
+			LOGGER.info('gotinfo: ' + JSON.stringify(val.updateInfo));
+		});
 		autoUpdater.on('update-available', () => {
 			browserWindow.webContents.send('update_available');
 		});
